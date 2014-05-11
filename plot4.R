5c3e5c8ce32b909d921448f6c8b32dfa0cf9e68f
AbidAliShaikh <abidalishaikh@hotmail.com>
2014-05-11 04:46:17 -0700

    project

---
+++ plot4.R
@@ -0,0 +1,20 @@
+setwd('c:/r/Git')
+library(sqldf)
+SqlQry <- "SELECT * from file WHERE Date ='1/2/2007' OR  Date='2/2/2007'"
+data <- read.csv.sql ('household_power_consumption.txt',SqlQry,sep=';')
+attach(data)
+
+par(mfrow=c(2,2))
+png (filename='plot4.png')
+hist(Global_active_power, main='Global Active Power', xlab="Global Active Power (kilowatts)",col='red')
+
+plot(data$DateTime, data$Voltage,type='l',ylab='Voltage',xlab='datetime')
+
+plot(data$DateTime, data$Sub_metering_1 ,type='l',ylab='Energy sub metering',xlab='')
+lines(data$DateTime, data$Sub_metering_2,col='red')
+lines(data$DateTime, data$Sub_metering_3,col='blue')
+legend ('topright', legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_2'),lwd=c(1,1,1),col=c('black','red','blue'))
+
+plot(data$DateTime, data$Global_reactive_power,type='l',xlab='datetime')
+
+dev.off()
